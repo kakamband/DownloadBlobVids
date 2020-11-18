7ee0d8032c2e12456e41be7e3a3c2e5d88a51920
kazin08 <53884911+kazin08@users.noreply.github.com>
2020-11-18 16:32:01 -0300

    Update video.sh

--- video.sh
+++ video.sh
@@ -2,7 +2,7 @@
 
 # set Hour Min Sec
 d=$(date +%Y-%m-%d-%H%M%S)
-folder="/home/emanuel/Videos/"$d".mkv"
+folder="/home/ubuntu/Videos/"$d".mkv"
 #echo $folder
 
 echo "Write the link to blob Video:"
